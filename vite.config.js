96a24def2419fe46fcc4e99f09e9ce1f4dc90104
Thuya-Myint <165346326+Thuya-Myint@users.noreply.github.com>
2025-06-23 15:05:31 +0630

    add auto open

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    open: true,
+    port: 1500,
+  }
 })
