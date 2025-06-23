c8e6d7a13bad597b9459cecd27d64b356058625f
Thuya-Myint <165346326+Thuya-Myint@users.noreply.github.com>
2025-06-23 14:26:03 +0630

    modified style

--- src/Body.jsx
+++ src/Body.jsx
@@ -11,6 +11,33 @@ const Body = () => {
       </div>
 
       <div className='w-full p-8 flex justify-between gap-2 ' >
+        <div className='transition-all duration-150 cursor-pointer  w-[400px] h-[350px] shadow-black/20 shadow-2xl bg-black/10 rounded-[45px]   hover:shadow-md'>
+
+          <div className='flex items-center gap-2 pl-5 pt-2'>
+            <div className='w-[6px] h-[6px] rounded-full bg-rod'>
+
+            </div>
+            <div className='font-light text-[1.2rem]'>
+              Gourmet Essentials
+            </div>
+
+          </div>
+
+          <div className='bg-black/20 w-full h-[75%] rounded-2xl mt-2 grid grid-cols-2 grid-rows-2 gap-2 p-2'>
+            <img src={s1} alt="" className='w-full  h-full rounded-xl' />
+            <img src={s2} alt="" className='w-full  h-full rounded-xl' />
+            <img src={s3} alt="" className='w-full  h-full rounded-xl' />
+            <img src={s4} alt="" className='w-full h-full rounded-xl' />
+
+          </div>
+
+          <button className='ml-20 mt-5 text-center  w-[200px] cursor-pointer h-[40px] border-2 border-rod px-6 py-1 rounded-full bg-cinnamon/80 text-white hover:bg-cinnamon/100 active:bg-cinnamon/50'>View Collection</button>
+
+        </div>
+
+
+
+
         <div className='w-[400px] h-[350px] bg-black/10 rounded-[45px] transition-transform duration-100 hover:scale-x-110'>
 
           <div className='flex items-center gap-2 pl-5 pt-2'>
@@ -20,25 +47,23 @@ const Body = () => {
             <div className='font-light text-[1.2rem]'>
               Gourmet Essentials
             </div>
-            
+
           </div>
- 
+
           <div className='bg-black/20 w-full h-[75%] rounded-2xl mt-2 grid grid-cols-2 grid-rows-2 gap-2 p-2'>
-            <img src={s1} alt="" className='w-full  h-full rounded-xl'/>
+            <img src={s1} alt="" className='w-full  h-full rounded-xl' />
             <img src={s2} alt="" className='w-full  h-full rounded-xl' />
             <img src={s3} alt="" className='w-full  h-full rounded-xl' />
             <img src={s4} alt="" className='w-full h-full rounded-xl' />
 
           </div>
-          
-          <button  className='ml-20 mt-5 text-center  w-[200px] h-[40px] border-2 border-rod px-6 py-1 rounded-full bg-rod/20 hover:bg-amber-500'>View Collection</button>
-          
+
+          <button className='ml-20 mt-5 text-center  w-[200px] h-[40px] border-2 border-rod px-6 py-1 rounded-full bg-rod/20 hover:bg-amber-500'>View Collection</button>
+
         </div>
 
 
-
-
-             <div className='w-[400px] h-[350px] bg-black/10 rounded-[45px] transition-transform duration-100 hover:scale-x-110'>
+        <div className='w-[400px] h-[350px] bg-black/10 rounded-[45px] transition-transform duration-100 hover:scale-x-110'>
 
           <div className='flex items-center gap-2 pl-5 pt-2'>
             <div className='w-[6px] h-[6px] rounded-full bg-rod'>
@@ -47,51 +72,26 @@ const Body = () => {
             <div className='font-light text-[1.2rem]'>
               Gourmet Essentials
             </div>
-            
+
           </div>
- 
+
           <div className='bg-black/20 w-full h-[75%] rounded-2xl mt-2 grid grid-cols-2 grid-rows-2 gap-2 p-2'>
-            <img src={s1} alt="" className='w-full  h-full rounded-xl'/>
+            <img src={s1} alt="" className='w-full  h-full rounded-xl' />
             <img src={s2} alt="" className='w-full  h-full rounded-xl' />
             <img src={s3} alt="" className='w-full  h-full rounded-xl' />
             <img src={s4} alt="" className='w-full h-full rounded-xl' />
 
           </div>
-          
-          <button  className='ml-20 mt-5 text-center  w-[200px] h-[40px] border-2 border-rod px-6 py-1 rounded-full bg-rod/20 hover:bg-amber-500'>View Collection</button>
-          
-        </div>
 
+          <button className='ml-20 mt-5 text-center  w-[200px] h-[40px] border-2 border-rod px-6 py-1 rounded-full bg-rod/20 hover:bg-amber-500'>View Collection</button>
 
-             <div className='w-[400px] h-[350px] bg-black/10 rounded-[45px] transition-transform duration-100 hover:scale-x-110'>
-
-          <div className='flex items-center gap-2 pl-5 pt-2'>
-            <div className='w-[6px] h-[6px] rounded-full bg-rod'>
-
-            </div>
-            <div className='font-light text-[1.2rem]'>
-              Gourmet Essentials
-            </div>
-            
-          </div>
- 
-          <div className='bg-black/20 w-full h-[75%] rounded-2xl mt-2 grid grid-cols-2 grid-rows-2 gap-2 p-2'>
-            <img src={s1} alt="" className='w-full  h-full rounded-xl'/>
-            <img src={s2} alt="" className='w-full  h-full rounded-xl' />
-            <img src={s3} alt="" className='w-full  h-full rounded-xl' />
-            <img src={s4} alt="" className='w-full h-full rounded-xl' />
-
-          </div>
-          
-          <button  className='ml-20 mt-5 text-center  w-[200px] h-[40px] border-2 border-rod px-6 py-1 rounded-full bg-rod/20 hover:bg-amber-500'>View Collection</button>
-          
         </div>
 
 
       </div>
 
 
-  
+
     </div>
   )
 }
--- src/HeaderBody.jsx
+++ src/HeaderBody.jsx
@@ -3,22 +3,22 @@ import React from 'react'
 const HeaderBody = () => {
   return (
     <div className='italiana pl-12 pt-32'>
-          <div className='italiana text-white  text-4xl border-l-4 border-rod pl-4'>
-            Where Flowers Meet Flour.
-          </div>
-          <div className='text-white text-2xl w-[22%] pt-6'>
-            Savor the scent, taste the petals and let every bite bloom with beauty.
-          </div>
-          <div className='text-white/60 text-lg pt-6'>
-            Step into a world where artisan baking is infused with the delicate charm of fresh bloom.
-          </div>
-          <div className='text-white text-lg flex gap-4 pt-4'> 
-            <div>
-              Visit us today. Taste the differece nature makes.
-            </div>
-            <button className='border-2 border-rod px-6 py-1 rounded-full bg-rod/20'>Go</button>
-          </div>
+      <div className='italiana text-white  text-4xl border-l-4 border-rod pl-4'>
+        Where Flowers Meet Flour.
+      </div>
+      <div className='text-white text-2xl w-[22%] pt-6'>
+        Savor the scent, taste the petals and let every bite bloom with beauty.
+      </div>
+      <div className='text-white/60 text-lg pt-6'>
+        Step into a world where artisan baking is infused with the delicate charm of fresh bloom.
+      </div>
+      <div className='text-white text-lg flex gap-4 pt-4'>
+        <div>
+          Visit us today. Taste the differece nature makes.
         </div>
+        <button className='border-2 border-rod px-6 py-1 rounded-full bg-rod/20'>Go</button>
+      </div>
+    </div>
   )
 }
 
