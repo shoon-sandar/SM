fab83ce4bfbe6021b4b5018bb4f33002aad94e6d
Thuya-Myint <165346326+Thuya-Myint@users.noreply.github.com>
2025-06-25 14:46:20 +0630

    commit

--- src/Body.jsx
+++ src/Body.jsx
@@ -3,10 +3,25 @@ import s2 from "./assets/Image/services/service1/butter.webp"
 import s3 from "./assets/Image/services/service1/freshBaguette.jpg"
 import s4 from "./assets/Image/services/service1/mozzarella.webp"
 
-import Example from "./Example"
+import s5 from "./assets/Image/services/service2/cake2.jpeg"
+import s6 from "./assets/Image/services/service2/cake1.webp"
+import s7 from "./assets/Image/services/service2/cake3.jpg"
+import s8 from "./assets/Image/services/service2/cake4.webp"
 
-import Card from './Card'
+import s9 from "./assets/Image/services/service3/cakeAndFlower1.jpg"
+import s10 from "./assets/Image/services/service3/cakeAndFlower2.webp"
+import s11 from "./assets/Image/services/service3/cakeAndFlower3.webp"
+import s12 from "./assets/Image/services/service3/flower1.webp"
+
+import Example from "./Example"
+import CardService1 from "./CardService1"
+import CardService2 from "./CardService2"
+import CardService3 from "./CardService3"
 const Body = () => {
+
+  const imgArrService1 = [s1, s2, s3, s4]
+  const imgArrService2 = [s5, s6, s7, s8]
+  const imgArrService3 = [s9, s10, s11, s12]
   return (
     <div className='w-screen h-screen'>
       <div className='border-l-4 border-l-rod text-[1.5rem] font-light pl-4  ml-4 mt-4'>
@@ -15,21 +30,15 @@ const Body = () => {
 
       <div className='w-full p-8 flex justify-between gap-2 ' >
 
-        <Card
-          s1={s1}
-          s2={s2}
-          s3={s3}
-          s4={s4}
+        <CardService1
+          imgArr={imgArrService1}
         />
-
-        <Example
-          p1={s1}
-          p2={s2}
-          p3={s3}
-          p4={s4}
+        <CardService2
+          imgArr={imgArrService2}
         />
-
-        
+        <CardService3
+          imgArr={imgArrService3}
+        />
 
       </div>
     </div>
---
+++ src/CardService3.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const CardService3 = ({ imgArr = [] }) => {
+  return (
+    <div className='transition-all duration-150  w-[400px] h-[350px] shadow-black/20 shadow-2xl bg-black/10 rounded-[45px]   hover:shadow-md'>
+
+      <div className='flex items-center gap-2 pl-5 pt-2'>
+        <div className='w-[6px] h-[6px] rounded-full bg-rod'>
+
+        </div>
+        <div className='font-thin text-[1rem]'>
+          Petals & Crumbs
+        </div>
+
+      </div>
+
+      <div className='bg-black/20 w-full h-[80%] rounded-2xl mt-2 grid grid-cols-2 grid-rows-2 gap-2 p-2'>
+        {
+          imgArr.map((img, index) => (
+            <img key={index} src={img} alt="" className='w-full h-full rounded-xl ' />
+          ))
+        }
+
+      </div>
+
+      <div className='w-full h-[20px] flex justify-center'>
+
+        <button className=' text-center  w-[200px] h-[40px] border-2  border-rod px-6 py-1 rounded-full bg-cinnamon/80 text-white hover:bg-cinnamon/100 active:bg-cinnamon/50 cursor-pointer'>View Collection</button>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default CardService3;
