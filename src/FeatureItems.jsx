bc5e97d4c48e8cd123ea64d648e7425940ac8270
shoon-sandar <sd801804@gmail.com>
2025-06-26 11:21:57 +0630

    amyint

--- src/FeatureItems.jsx
+++ src/FeatureItems.jsx
@@ -1,31 +1,38 @@
 import React from 'react'
 
-const FeatureItems = ({fitems}) => {
-  return (
-    <div className='w-full h-full p-8'>
-        <div className='mb-5'>
-            ⭐️ Best Seller Items
-        </div>
-    <div className='w-full flex justify-between'>
-        {
-            fitems.map((item,index)=>(
-                <div key={index} className='bg-black w-[250px] h-[300px]'>
+const FeatureItems = ({ fitems }) => {
+    return (
+        <div className='w-full h-full p-8'>
+            <div className='mb-5'>
+                ⭐️ Best Seller Items
+            </div>
+            <div className='w-full flex justify-between'>
+                {
+                    fitems.map((item, index) => (
+                        <div key={index} className=' w-[250px] h-[300px] shadow-lg shadow-black/30 '>
 
-                    <img src={item.image} alt="" className='w-[250px] h-[250px] rounded-t-2xl' />
-                    <div className='bg-white p-2'>
-                    <div className=' rounded-b-2xl shadow-lg shadow-black/30 '>
-                        {item.title}
-                    </div>
-                    <div>
-                       ⭐️ 
-                    </div>
-                    </div>
-                </div>
-            ))
-        }
-    </div>
-    </div>
-  )
+                            <img src={item.image} alt="" className='w-[250px] h-[250px] rounded-t-2xl' />
+                            <div className='bg-white p-2  rounded-b-2xl'>
+                                <div className=' rounded-b-2xl font-light'>
+                                    {item.title}
+                                </div>
+
+                                <div className='flex justify-between items-center'>
+                                    <div className='font-light text-black/50'>
+                                        Rating
+                                    </div>
+                                    <div>
+                                        ⭐️⭐️⭐️⭐️
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
 }
 
 export default FeatureItems
