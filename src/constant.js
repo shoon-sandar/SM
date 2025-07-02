8e7a23f4f8c32e69b488f372accfbf8a5cf26411
Thuya-Myint <165346326+Thuya-Myint@users.noreply.github.com>
2025-07-02 15:59:10 +0630

    ayan chit tl kalay lay

--- src/constant.js
+++ src/constant.js
@@ -1,86 +1,89 @@
 import c1 from './assets/Image/featuredItems/f2.jpg'
 import c2 from './assets/Image/featuredItems/f4.jpg'
 export const arr = [
-    {
-        name: "Strawberry Paradise Cake",
-        price: 250000,
-        img: c1,
-    },
-    {
-        name: "Strawberry Paradise Cake",
-        price: 250000,
-        img: c1,
-    },
-    {
-        name: "Strawberry Paradise Cake",
-        price: 250000,
-        img: c1,
-    },
-    {
-        name: "Strawberry Paradise Cake",
-        price: 250000,
-        img: c1,
-    },
-    {
-        name: "Strawberry Paradise Cake",
-        price: 250000,
-        img: c1,
-    },
-    {
-        name: "Strawberry Paradise Cake",
-        price: 250000,
-        img: c1,
-    },
+  {
+    name: "Strawberry Paradise Cake",
+    price: 250000,
+    img: c1,
+  },
+  {
+    name: "Strawberry Paradise Cake",
+    price: 250000,
+    img: c1,
+  },
+  {
+    name: "Strawberry Paradise Cake",
+    price: 250000,
+    img: c1,
+  },
+  {
+    name: "Strawberry Paradise Cake",
+    price: 250000,
+    img: c1,
+  },
+  {
+    name: "Strawberry Paradise Cake",
+    price: 250000,
+    img: c1,
+  },
+  {
+    name: "Strawberry Paradise Cake",
+    price: 250000,
+    img: c1,
+  },
 
 
 ]
 export const sale = [
-    {
-        name: "Strawberry Paradise Cake",
-        price: 250000,
-        img: c2,
-    },
-    {
-        name: "Strawberry Paradise Cake",
-        price: 250000,
-        img: c2,
-    },
-    {
-        name: "Strawberry Paradise Cake",
-        price: 250000,
-        img: c2,
-    },
+  {
+    name: "Strawberry Paradise Cake",
+    price: 250000,
+    img: c2,
+  },
+  {
+    name: "Strawberry Paradise Cake",
+    price: 250000,
+    img: c2,
+  },
+  {
+    name: "Strawberry Paradise Cake",
+    price: 250000,
+    img: c2,
+  },
 
 
 ]
-export const today=[
-    {
-        title: "Strawberry Cake",
-        time:"05:00:00",
-        disprice:"20000",
-        oriprice:"25000",
-        img:c1,
-    },
-    {
-        title: "Strawberry Cake",
-        time:"05:00:00",
-        disprice:"20000",
-        oriprice:"25000",
-        img:c1,
-    },
-    {
-        title: "Strawberry Cake",
-        time:"05:00:00",
-        disprice:"20000",
-        oriprice:"25000",
-        img:c1,
-    },
-    {
-        title: "Strawberry Cake",
-        time:"05:00:00",
-        disprice:"20000",
-        oriprice:"25000",
-        img:c1,
-    },
+export const today = [
+  {
+    title: "Strawberry Cake",
+    time: "05:00:00",
+    disprice: "20000",
+    oriprice: "25000",
+    img: c1,
+    rating: 4
+  },
+  {
+    title: "Strawberry Cake",
+    time: "05:00:00",
+    disprice: "20000",
+    oriprice: "25000",
+    img: c1,
+    rating: 5
+  },
+  {
+    title: "Strawberry Cake",
+    time: "05:00:00",
+    disprice: "20000",
+    oriprice: "25000",
+    img: c1,
+    rating: 3
+  },
+  {
+    title: "Strawberry Cake",
+    time: "05:00:00",
+    disprice: "20000",
+    oriprice: "25000",
+    img: c1,
+    rating: 3
+  },
 ]
-
--- src/newItem.jsx
+++ src/newItem.jsx
@@ -1,35 +1,46 @@
 import React from 'react'
 import { TiStarOutline } from "react-icons/ti";
 const newItem = ({ newArr }) => {
-    return (
-        <div className='mt-10'>
-            <div className='text-2xl font-light border-l-4 border-black pl-2'>
-                New Arrivals
-            </div>
-            <div className='grid grid-cols-4 p-6 grid-rows-1 gap-6'>
-                {
-                    newArr.map((item, index) => (
-                        <div
-                            key={index}
-                            className='w-full h-full '
-                        >
+  const starCount = [0, 1, 2, 3, 4]
+  return (
+    <div className='mt-10'>
+      <div className='text-2xl font-light border-l-4 border-black pl-2'>
+        New Arrivals
+      </div>
+      <div className='grid grid-cols-4 p-6 grid-rows-1 gap-6'>
+        {
+          newArr.map((item, index) => (
+            <div
+              key={index}
+              className='w-full h-full '
+            >
 
-                            <img src={item.img} alt="" className='w-full h-[250px]   m-auto mt-2 shadow-xl shadow-black/20' />
-                            <div className='mt-2'>
-                                <div className='font-light text-xl'>{item.title}</div>
-                                <div className='flex items-items-center justify-between '>
-                                    <div className=' text-md text-black/60'>
-                                        {item.oriprice}
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                    ))
-                }
+              <img src={item.img} alt="" className='w-full h-[250px]   m-auto mt-2 shadow-xl shadow-black/20' />
+              <div className='mt-2'>
+                <div className='font-light text-xl'>{item.title}</div>
+                <div className='flex items-items-center justify-between '>
+                  <div className=' text-md text-black/60'>
+                    {item.oriprice}
+                  </div>
+                  <div>
+                    {
+                      starCount.map((i) => (
+                        i < item.rating ? (
+                          <TiStarOutline key={i} className="text-yellow-500 text-lg" />
+                        ) : (
+                          <TiStarOutline key={i} className="text-gray-400 text-lg" />
+                        )
+                      ))
+                    }
+                  </div>
+                </div>
+              </div>
             </div>
-        </div>
-    )
+          ))
+        }
+      </div>
+    </div>
+  )
 }
 
 export default newItem
